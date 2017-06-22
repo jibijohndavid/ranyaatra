323086826f1cc208a5ebe00021d8c2d68208d566
stefan <stefan@qburst.com>
2017-06-22 18:37:04 +0530

    cart popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -10,7 +12,7 @@ import { ContactComponent }     from './contact-component/contact.component';
 import { PackageService }       from './package-service/package.service';
 import { PackageComponent }     from './package-component/package.component';
 import { DestinationComponent }     from './destination-component/destination.component';
-import { HeaderComponent }      from './header-component/header.component';
+import { HeaderComponent, DialogContent}      from './header-component/header.component';
 import { CartService }          from './package-service/cart.service';
 import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './detail/detail.component';
@@ -26,15 +28,19 @@ import { DetailComponent } from './detail/detail.component';
     DestinationComponent,
     HeaderComponent,
     HomeComponent,
-    DetailComponent
+    DetailComponent,
+    DialogContent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [PackageService,CartService],
-  bootstrap: [AppComponent]
+  entryComponents: [DialogContent],
+  bootstrap: [AppComponent, HeaderComponent]
 })
 export class AppModule { }
--- src/app/header-component/header.component.ts
+++ src/app/header-component/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit }  from '@angular/core';
+import { Component, OnInit, Optional}  from '@angular/core';
 import { CartService }        from '../package-service/cart.service';
+import { MdDialog, MdDialogRef } from '@angular/material';
 
 declare var jquery:any;
 declare var $ :any;
@@ -11,10 +12,16 @@ declare var $ :any;
 })
 export class HeaderComponent implements OnInit {
 
-  constructor (private cartService: CartService) {
+
+  constructor (
+    private cartService: CartService,
+    private _dialog: MdDialog,
+  ) {
     this.cartService = cartService;
   }
 
+  isDarkTheme: boolean = false;
+  lastDialogResult: string;
   cart = {
     items:[],
     count:0
@@ -24,11 +31,27 @@ export class HeaderComponent implements OnInit {
     this.cart = this.cartService.getCartItems();
   }
 
-  viewCart () {
-    console.log(this.cart);
+  openDialog() {
+    this._dialog.open(DialogContent);
   }
 
   ngOnInit(): void {
     this.getCartItems();
   }
+}
+
+@Component({
+  template: `
+    <p>This is a dialog</p>
+    <p>
+      <label>
+        This is a text box inside of a dialog.
+        <input #dialogInput>
+      </label>
+    </p>
+    <p> <button md-button (click)="dialogRef.close(dialogInput.value)">CLOSE</button> </p>
+  `,
+})
+export class DialogContent {
+  constructor(@Optional() public dialogRef: MdDialogRef<DialogContent>) { }
 }
